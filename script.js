0b352704b3c0f70c5d5804e1f8665649b8c1e3c6
mamidalaramu <ramukumarlucky@gmail.com>
2023-04-14 13:08:59 +0530

    Update the Home page, Add about me section

--- script.js
+++ script.js
@@ -39,3 +39,17 @@ var icon = document.getElementById("icon");
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
   };
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+  for(tablink of tablinks){
+    tablink.classList.remove("active-link");
+  }
+  for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
