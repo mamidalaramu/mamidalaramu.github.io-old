e1fa845da29daad70e7c6ea734dc857e39a57bbe
mamidalaramu <ramukumarlucky@gmail.com>
2023-04-17 20:03:06 +0530

    Add Contact page, updated the dark mode, made some tiny changed to responsive UI

--- pages/projects.js
+++ pages/projects.js
@@ -5,10 +5,15 @@ canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext('2d');
 
+function fillRe(){
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 function setBackgroundColor(color) {
   
   ctx.fillStyle = color;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  fillRe();
+  
 }
 
 var colors = ["#FF6138", "#FFBE53", "#2980B9", "#282741"];
@@ -18,6 +23,30 @@ canvas.addEventListener('click', function() {
   var color = colors[Math.floor(Math.random()*colors.length)]
   
   setBackgroundColor(color);
+  // animate();
 });
 
-console.log(ctx);
+// var x = 200;
+// var y = 200;
+// var radius =30;
+// dx = 4;
+// dy = 4;
+// function animate(){
+//   requestAnimationFrame(animate);
+//   fillRe();
+//   ctx.beginPath();
+//   ctx.arc(x,y,radius,Math.PI*2,false);
+//   ctx.strokeStyle = 'blue';
+//   ctx.stroke();
+//   if(x +radius > innerWidth || x -radius < 0){
+//     dx = -dx;
+//   }
+//   x += dx;
+//   if(y +radius > innerHeight || y -radius < 0){
+//     dy = -dy;
+//   }
+//   y+=dy;
+// }
+// console.log(animate)
+
+// console.log(ctx);
